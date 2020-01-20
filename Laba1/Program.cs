6f3c22a99c74b40bdd243d7b371b6eaef13eec18
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 00:45:39 +0300

    Добавьте файлы проекта.

---
+++ Laba1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" +\"\"\"\"\"+ \n");
+            Console.Write("(| o o |) \n");
+            Console.Write(" |  ^  |  \n");
+            Console.Write(" | '-' | \n");
+            Console.Write(" +-----+ \n");
+        }
+    }
+}

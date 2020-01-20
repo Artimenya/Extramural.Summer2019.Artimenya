49471b5ad77f94a4600c0f881b2fb2b4599d0440
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 00:01:33 +0300

    Добавьте файлы проекта.

---
+++ Lab0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the First Number: ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the Second Number: ");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int tmp = val1;
+            val1 = val2;
+            val2 = tmp;
+
+            Console.Write("After Swapping: \n");
+            Console.Write("First Number: " + val1 + "\n");
+            Console.Write("Second Number: " + val2 + "\n");
+        }
+    }
+}

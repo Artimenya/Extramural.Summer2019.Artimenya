25102434153aef8795ec2c502f2e5365c3bbd1ec
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:36:11 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to reverse a string (use class String, use ckass StringBuilder).
+            Console.WriteLine("Input a string: ");
+            string val = Console.ReadLine();
+            int l = val.Length - 1;
+            string newstring = "";
+            while (l >= 0)
+            {
+                newstring = newstring + val[l];
+                l--;
+            }
+
+            Console.WriteLine("Reverse string: " + newstring);
+        }
+    }
+}

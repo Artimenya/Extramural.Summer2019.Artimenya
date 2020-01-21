3f07ca2cc3cf1d82e58f637fa633572b7d4a2af2
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:56:04 +0300

    Добавьте файлы проекта.

---
+++ Lab9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to find the first N prime numbers.
+            Console.WriteLine("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int c, i = 0, count;
+            int[] array = new int[100];
+
+            for (count = 0; count < num; i++)
+            {
+                // iteration to check c is prime or not
+                for (c = 2; c < i; c++)
+                {
+                    if (i % c == 0)
+                        break;
+                }
+
+                if (c == i)  // c is prime
+                {
+                    array[count] = i;
+                    count++;    // increment the count of prime numbers
+                }
+
+            }
+            Console.WriteLine("First " + num + " prime numbers: ");
+            for (i = 0; i < count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}

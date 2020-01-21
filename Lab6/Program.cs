eef16becd3fc60ce6fdcc1bb7786c4c1d5e8d399
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:44:57 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            String wday = time.DayOfWeek.ToString();
+            String month = time.ToString("MMMM");
+            String date = time.Day.ToString();
+            String year = time.Year.ToString();
+            Console.WriteLine("Current Date time: " + wday + ", " + month + " " + date + ", " + year);
+        }
+    }
+}

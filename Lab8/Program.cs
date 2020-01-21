f0877190d044c5acf1a5c4fab3605d27329807a1
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:52:48 +0300

    Добавьте файлы проекта.

---
+++ Lab8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to capitalize the first letter of each word in a sentence.
+            //the quick brown fox jumps over the lazy dog.
+            Console.WriteLine("Test Data:");
+            String val = Console.ReadLine();
+
+            val = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(val);
+            Console.WriteLine("Result string: " + val);
+        }
+    }
+}

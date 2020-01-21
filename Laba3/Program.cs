7976b1bc358bdb2227f3beb591b286683d11e677
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:32:43 +0300

    Добавьте файлы проекта.

---
+++ Laba3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to convert a hexadecimal to a decimal number.
+            Console.WriteLine("Enter a hexadecimal number: ");
+            string hnumber = Console.ReadLine();
+            int decnumber = int.Parse(hnumber, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("Decimal number: " + decnumber);
+        }
+    }
+}

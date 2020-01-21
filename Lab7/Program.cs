9b461045deca5d86808955acb1128d59ead4270e
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:47:40 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            String date = time.ToString(DateTime.Now.ToString("MM/dd/yyyy"));
+
+            Console.WriteLine("Current Date time: " + date);
+        }
+    }
+}

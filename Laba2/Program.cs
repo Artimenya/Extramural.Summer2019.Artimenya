ee228b1666347a3bb4d5715fb9902e594c690a7f
Нюта Фран <Hanna_f06@mail.ru>
2020-01-21 13:18:05 +0300

    Добавьте файлы проекта.

---
+++ Laba2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i;
+            int[] array = new int[20];
+            Console.Write("Enter  decimal number: ");
+            num = int.Parse(Console.ReadLine());
+            for (i = 0; num > 0; i++)
+            {
+                array[i] = num % 2;
+                num = num / 2;
+            }
+            Console.Write("Binary number: ");
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+            }
+        }
+    }
+}
